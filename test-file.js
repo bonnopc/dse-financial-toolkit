9958f19b8ed758479203943ccb5146c0126bb690
Prosenjit Chowdhury <pc.chy007@gmail.com>
2025-08-03 09:02:20 +0600

    Complete prettier, lint-staged, and husky setup
    - Add comprehensive scripts for all sub-projects
    - Configure lint-staged to format code before commits
    - Set up working pre-commit hook
    - Test formatting with prettier

--- test-file.js
+++ test-file.js
@@ -1,5 +1,5 @@
 // Test file to verify pre-commit hooks are working
 const testFunction = () => {
-console.log('This should be formatted by prettier');
+  console.log('This should be formatted by prettier');
   return 'test';
 };
