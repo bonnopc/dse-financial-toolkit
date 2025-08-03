f7bf2b13cba8e70712cf37b225596b62824cda07
Prosenjit Chowdhury <pc.chy007@gmail.com>
2025-08-03 09:12:02 +0600

    Test commit that should fail due to failing tests

---
+++ dse-api/src/test-api-commit.ts
@@ -0,0 +1,5 @@
+// Test file for DSE API to trigger tests and linting
+export function testFunction() {
+  console.log('Testing API linting and tests');
+  return true;
+}
---
+++ test-commit-hook.js
@@ -0,0 +1,8 @@
+// Test file with intentional formatting issues to test pre-commit hooks
+const testFunction = () => {
+  console.log('This has bad formatting');
+  const obj = { a: 1, b: 2 };
+  return obj;
+};
+
+export default testFunction;
