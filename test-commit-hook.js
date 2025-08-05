57a84a01b02352e5b9958006166093e387e03ebf
Prosenjit Chowdhury <pc.chy007@gmail.com>
2025-08-06 00:59:39 +0600

    removed unnecessary files

--- test-commit-hook.js
+++
@@ -1,8 +0,0 @@
-// Test file with intentional formatting issues to test pre-commit hooks
-const testFunction = () => {
-  console.log('This has bad formatting');
-  const obj = { a: 1, b: 2 };
-  return obj;
-};
-
-export default testFunction;
